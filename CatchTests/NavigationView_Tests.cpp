e7dc558d79af220eb4cc2236c3abe151e12d14c4
morle <upupwrite@outlook.com>
2026-09-13 11:28:35 +0800

    Update catch to catch_all head files

--- CatchTests/NavigationView_Tests.cpp
+++ CatchTests/NavigationView_Tests.cpp
@@ -2,7 +2,7 @@
 #if __has_include(<catch2/catch_test_macros.hpp>)
 #  include <catch2/catch_test_macros.hpp>
 #else
-#  include <catch2/catch.hpp>
+#  include <catch2/catch_all.hpp>
 #endif
 
 #include "navigationview.h"
